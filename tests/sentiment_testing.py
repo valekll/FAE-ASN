66434a2575076977e80bcf619cb74878818e7290
Jonathan Jones <me@xayah.net>
2020-02-08 09:08:31 -0500

    use compound score

--- tests/sentiment_testing.py
+++ tests/sentiment_testing.py
@@ -5,20 +5,15 @@ import nltk
 x_da = pd.read_csv("data/xayah_randomized.csv")
 x_da.dropna()
 
-train = pd.read_csv("data/labeledTrainData.tsv", header=0, delimiter="\t", quoting=3)
-
 import random
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from sklearn.metrics import accuracy_score
 
 vader = SentimentIntensityAnalyzer()
-def vader_polarity(text):
-    score = vader.polarity_scores(text)
-    return 1 if score['pos'] > score['neg'] else 0
 
 results = [vader.polarity_scores(text) for text in x_da['Text']]
 
 for i in results:
-    print(i)
+    print(i['compound'])
     print("\n")
