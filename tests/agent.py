c01ce7453c54dde1333f9d66c67c3cc9ad9c6e60
valekll <45467718+valekll@users.noreply.github.com>
2020-02-08 10:57:34 -0500

    Changed json file

--- tests/agent.py
+++ tests/agent.py
@@ -5,3 +5,63 @@ Created on Sat Feb  8 09:11:26 2020
 @author: lordv
 """
 
+import os
+import json
+
+class Agent:
+    def __init__(self, data):
+        self.__dict__ = data
+        
+def findAgents():
+    with open('data/agents.json', 'r') as agentFile:
+        agentsJSON = json.load(agentFile)
+       
+        agents = []
+        for agentJSON in agentsJSON:
+            agentx = Agent(agentJSON)
+            agents.append(agentx)
+        
+        print('Found the following agents: ')
+        for agentx in agents:
+            print(agentx.__dict__)
+        print('\n')
+        
+        return agents
+
+def writeAgents(agents):
+    print('Writing the following agents: ')
+    for agentx in agents:
+            print(agentx.__dict__)
+    print('\n')
+                
+    with open('data/agents.json', 'w') as agentFile:
+        agentFile.write('[\n')
+    
+    i = 1
+    for agentx in agents:
+        with open('data/agents.json', 'a') as agentFile:
+            json.dump(agentx.__dict__, agentFile)
+            if i < len(agents):
+                agentFile.write(',')
+            agentFile.write('\n')
+        i += 1
+    
+    with open('data/agents.json', 'a') as agentFile:
+        agentFile.write(']')
+
+def createAgents():
+    agents = []
+    dict = { "name" : "", "TOKEN" : "", "chatRate" : ""}
+    for i in range(30):
+        agentx = Agent(dict)
+        agents.append(agentx)
+        
+    print('Created the following agents: ')
+    for agentx in agents:
+            print(agentx.__dict__)
+    print('\n')
+    return agents
+    
+#agents = findAgents()
+#agents = createAgents()
+#writeAgents(agents)
