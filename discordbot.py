cfeaa3135fafefa3d7a6505ccf31979f4f3654a3
valekll <45467718+valekll@users.noreply.github.com>
2020-02-08 17:48:23 -0500

    discordboglogin added

--- discordbot.py
+++ discordbot.py
@@ -10,7 +10,7 @@ import discord
 import time
 
 def genMessage(oldmsg, agentx):
-    return 'canned response'
+    return 'reed is a nerd'
 
 agents = agent.findAgents()
 
@@ -31,8 +31,8 @@ def runBot(agentx):
         print('------')
         channel = client.get_channel(675698151935442967)
         await channel.send('I am here.')
-        await client.logout()
-    '''    
+
+        
     @client.event
     async def on_message(message):
         if message.author == client.user:
@@ -42,13 +42,8 @@ def runBot(agentx):
             oldmsg = ''
             newmsg = genMessage(oldmsg, agentx)
             await channel.send(newmsg)
-            await client.logout()
-            time.sleep(.2)
-    '''        
+        
+            
     client.run(TOKEN)
 
-i = 0
-while i < 10:
-    runBot(agents[i])
-    i += 1
-    time.sleep(.2)
+runBot(agents[0])
--- discordbotlogin.py
+++ discordbotlogin.py
@@ -9,37 +9,25 @@ import agent
 import discord
 import time
 
-def genMessage(oldmsg, agentx):
-    return 'test'
-
 agents = agent.findAgents()
 
 TOKEN = 'XXXXXXXXXX'
 for agentx in agents:
     if agentx.TOKEN == '':
         agents.remove(agentx)
-for agentx in agents:        
+        
+def runBot(agentx):    
     TOKEN = agentx.TOKEN
-
     client = discord.Client()
-
+    
     @client.event
     async def on_ready():
         print('Logged in as')
         print(client.user.name)
         print(client.user.id)
         print('------')
-
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
-        channel = client.get_channel(675698151935442967)
-        if channel == message.channel:
-            oldmsg = ''
-            newmsg = genMessage(oldmsg, agentx)
-            await channel.send(newmsg)
-            await client.logout()
-            time.sleep(.2)
+       
             
     client.run(TOKEN)
+
+runBot(agents[0])
